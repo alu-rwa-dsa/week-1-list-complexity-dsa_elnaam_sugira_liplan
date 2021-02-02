948216ebcd05b89917d80c11730931e3f49739c6
Elnaam10 <e.umutoni@alustudent.com>
2021-02-02 11:16:50 -0800

    num3

---
+++ question3b.py
@@ -0,0 +1,9 @@
+
+
+def ToLower(word):
+    return word.lower()
+
+
+# word = 'Three'
+
+# print(word.lower())
---
+++ question3c.py
@@ -0,0 +1,10 @@
+
+def SortLs(ls):
+    ls.sort()
+    return ls
+
+# ls = [1, 3, 4, 3, 5, 6, 7, 66, 43, 23]
+
+# ls.sort()
+
+# print(ls)
